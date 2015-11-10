68d6e6bd319ed83c456654e882047adab48af871
Jared <jronnen7@gmail.com>
2015-11-09 22:22:54 -0700

    updates working-version 0.1.2 more updates soon

--- responsive-tables.js
+++ responsive-tables.js
@@ -4,7 +4,7 @@ function ResponsiveTables() {
 
 	var tables = document.getElementsByTagName("table");
 	for(var i=0;i<tables.length;i++){
-		new ResponsiveTable(tables[i]);
+		new ResponsiveTable(tables[i]).Respond();
 	}
 }
 
@@ -44,38 +44,29 @@ ResponsiveTable.prototype.Respond = function () {
 }
 
 ResponsiveTable.prototype.ShouldShrink = function () {
-	/* TODO */
-	return this.CountColumns()>3;
+
+	return window.innerWidth < this.table.clientWidth;
 }
 
 ResponsiveTable.prototype.ShouldExpand = function () {
-	/* TODO */
-	return false;
+	return window.innerWidth > this.table.clientWidth;
 }
 
 
-ResponsiveTable.prototype.GetNumberOfColumns = function () {
-	var ret = 3;
-	/* TODO */
-	return ret;
-}
 
-ResponsiveTable.prototype.CountColumns = function (i) {
-	return this.tableRows[0].getElementsByTagName('td').length;
+ResponsiveTable.prototype.CountColumns = function () {
+	return this.tableRows[0].getElementsByTagName('th').length;
 }
 
 ResponsiveTable.prototype.RemoveLeastImportantColumn = function () {
 	/* TODO get table head // remove and store to variable */	
-	for(var i=1;i<this.tableRows[i].length;i++) {
-		if(!this.IsGeneratedRow()) {
-			var column = this.GetLeastImportantColumn(i);
-			this.RemoveFromDisplay(column, i);
-			if(!this.IsGeneratedColumnNext(i)) {
-				this.GenerateRow(i);
-			} 
-			this.InsertBelow(column, i);
-		}
-	}
+	
+
+	var columnNum = this.GetLeastImportantColumn();
+	var removed = this.RemoveFromDisplay(columnNum);
+	this.InsertBelow(removed);
+
+
 }
 
 ResponsiveTable.prototype.AddMostImportantColumn = function () {
@@ -86,41 +77,70 @@ ResponsiveTable.prototype.IsGeneratedRow = function (i) {
 	return this.tableRows[i].className.indexOf('generatedRow') > -1;
 }
 
-ResponsiveTable.prototype.IsGeneratedColumnNext = function (i) {
+ResponsiveTable.prototype.IsGeneratedRowNext = function (i) {
 	var ret = false;
 	if(this.tableRows.length > i + 1) {
 		ret = this.tableRows[i+1].className.indexOf('generatedRow') > -1;
 	} 
 	return ret;
 }
-ResponsiveTable.prototype.GetMostImportantColumn = function (i) {
+ResponsiveTable.prototype.GetMostImportantColumn = function () {
 	var ret = null; var priority = 0;
 	return ret;
 }
 
-ResponsiveTable.prototype.GetLeastImportantColumn = function (i) {
+ResponsiveTable.prototype.GetLeastImportantColumn = function () {
 	var ret = null; var priority = 0;
-	var row = this.tableHead;
+	var row = this.tableRows[0];
 	var leastImpt = 0;
 	for(var j=0;j<row.children.length;j++) {
-		var temp = row.children[j].getAttribute("data-priority");
+		var temp = Number(row.children[j].getAttribute("data-priority"));
 		if(temp!==undefined && temp!=null && temp > priority) {
+			priority = temp;
 			leastImpt = j;
 		}
-	} return this.tableRows[i].children[leastImpt];
+	} return leastImpt;
 }
 
-ResponsiveTable.prototype.RemoveFromDisplay = function (column, i) {
-	this.tableRows[i].removeChild(column);
+ResponsiveTable.prototype.RemoveFromDisplay = function (columnNumber) {
+	this.columnHeader = this.tableRows[0].children[columnNumber].innerHTML;
+	this.currentPriority = this.tableRows[0].children[columnNumber].getAttribute("data-priority");
+	var ret = new Array(); var j=0;
+	for(var i=0;i<this.tableRows.length;i++) {
+		if(!this.IsGeneratedRow(i)) {
+			ret[j] = this.tableRows[i].removeChild(this.tableRows[i].children[columnNumber]);
+			j++;
+		}
+	} return ret;
 }
-ResponsiveTable.prototype.InsertBelow = function (column, i) {
-	this.tableRows[i+1].innerHTML += column.innerHTML;
+ResponsiveTable.prototype.InsertBelow = function (removed) {
+	// Skip first row (head)
+	var j=1;	
+	for(var i=1;i<this.tableRows.length;i++) {
+		if(!this.IsGeneratedRow(i)) {
+			if(!this.IsGeneratedRowNext(i)) {
+				this.GenerateRow(i);	
+			}
+			var elmToAdd = document.createElement('p');
+			elmToAdd.setAttribute("data-priority", this.currentPriority);
+			elmToAdd.innerHTML = "<span class='title' >" + removed[0].innerHTML + "</span>" +':'+ removed[j].innerHTML + "<br/>";
+			this.tableRows[i+1].children[0].appendChild(elmToAdd);
+			this.tableRows[i+1].children[0].colSpan = this.CountColumns();
+			j++;
+		}
+	}
 }
-ResponsiveTable.prototype.GenerateRow = function (column, i) {
+ResponsiveTable.prototype.GenerateRow = function (i) {
+	var genTd = document.createElement('td');
 	var genTr = document.createElement('tr');
-	genTr.innerHTML = column;
 	genTr.style.display = "none";
-	this.table.children[0].insertBefore(genTr, this.tableRows[i]);
+	genTr.className = "generatedRow";
+	genTr.appendChild(genTd);
+	if(this.tableRows.length == i) {
+		this.table.children[0].append(genTr);	
+	} else {
+		this.table.children[0].insertBefore(genTr, this.tableRows[i+1]);
+	}
 }
 
 
